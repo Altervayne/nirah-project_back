ae2a94a7c591881b6670909912031b4dd72c5f10
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-04-21 19:48:19 +0200

    fixed routes controller calls

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,11 @@ const userControl = require('../controllers/user')
 
 
 
+router.get('/', auth,  userControl.getCurrentUserInfo)
+router.get('/friend/:id', auth, userControl.getFriendsStatus)
 router.post('/signup', userControl.signUp)
 router.post('/login', userControl.logIn)
-router.post('/sendRequest/:id', userControl.logIn)
+router.post('/sendRequest/:id', auth, userControl.requestFriend)
 
 
 
