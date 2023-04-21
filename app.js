7f1daaa204cd855b960f18c1a1317219e57725a7
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-04-21 16:52:32 +0200

    set up base server.js file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+
+
+
+const app = express();
+
+
+
+app.use(express.json())
+
+
+
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+})
