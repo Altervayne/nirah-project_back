22ca4c914c703ff4d57cd6ba8b3a5598c724041b
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-05-10 03:17:35 +0200

    commented socket.js file

--- socket.js
+++ socket.js
@@ -10,6 +10,7 @@ let io
 
 
 exports.init = (server) => {
+    /* We initiate the websockets by making sure credentials are used */
     io = socketio(server, {
             cors: {
             origin: process.env.CLIENTADDRESS,
@@ -17,27 +18,32 @@ exports.init = (server) => {
             credentials: true,
         }
     })
+
+    /* We make use of the auth middleware to authenticate users and after that we initiate the connection */
     io.use(auth.socket)
     io.on('connection', onConnection)
 }
 
+
+
 function onConnection(socket) {
+    /* We log the succesful connection in the console and map the user's Id to their socketId */
     console.log(`Socket successfully connected: ${socket.id}`)
     console.log(`Connected user: ${socket.auth.username}`)
     console.log(`userId: ${socket.auth.userId}`)
     userIdToSocketIdMap.set(socket.auth.userId, socket.id)
 
+
+    /* We set the user's online state to true and notify their friends that they have come online */
     socketUser.setUserOnline(socket, true)
     socketFriends.friendConnectionUpdate(socket, io, userIdToSocketIdMap, 'connected')
 
 
-    
-    /* Basic Join/Leave room controllers */
 
+    /* Basic Join/Leave room controllers */
     socket.on('joinRoom', (data) => {
         socketRoom.joinRoom(socket, io, users, userIdToSocketIdMap, data)
     })
-
     socket.on('leaveRoom', (data, callback) => {
         socketRoom.leaveRoom(socket, io, users, userIdToSocketIdMap, data, callback)
     })
@@ -45,7 +51,6 @@ function onConnection(socket) {
 
 
     /* Room messaging controllers */
-
     socket.on('sendMessage', (data) => {
         socketRoom.sendMessage(socket, io, users, data)
     })
@@ -53,7 +58,6 @@ function onConnection(socket) {
 
 
     /* Friend requests controllers */
-
     socket.on('friendRequest', (data) => {
         socketFriends.friendRequest(socket, io, userIdToSocketIdMap, data)
     })
@@ -61,7 +65,6 @@ function onConnection(socket) {
 
 
     /* Basic Disconnect/Error controllers */
-
     socket.on('disconnect', async () => {
 
         /* Get user's friends' Ids and map them to their socket IDs, then send leaveRoom event to notify them */
@@ -84,7 +87,6 @@ function onConnection(socket) {
         /* Indicate that disconnection is complete */
         console.log(`Socket successfully disconnected: ${socket.id}`)        
     })
-
     socket.on('error', (error) => {
         console.error(`Error with socket ${socket.id}:`, error)
     })
